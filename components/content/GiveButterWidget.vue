42c71b88c3992fae7760164d5b596556ac0cbca2
Sarad Subedi <sarad.subedi@kotuko.it>
2024-08-06 16:30:43 +0545

    first commit

---
+++ components/content/GiveButterWidget.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="centered-widget">
+    <givebutter-widget id="LYdEKj"></givebutter-widget>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiveButterWidget",
+  mounted() {
+    const script = document.createElement("script");
+    script.src =
+      "https://widgets.givebutter.com/latest.umd.cjs?acct=JIchSptaQ0kQJOkZ&p=other";
+    script.async = true;
+    document.body.appendChild(script);
+    script.onload = () => {
+      // Initialize the Givebutter widget here if needed
+      // Example: GivebutterWidget.init({target: "#givebutter-widget"});
+    };
+  },
+};
+</script>
+
+<style scoped>
+.centered-widget {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+}
+</style>
